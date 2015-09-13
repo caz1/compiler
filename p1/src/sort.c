dac98a5ea7907d701d1561a065b03f66784252a3
caz <caz@mail.ustc.edu.cn>
2015-09-13 18:09:19 +0800

    add p2

--- p1/src/sort.c
+++ p1/src/sort.c
@@ -1,21 +1,24 @@
+
+
 int a[] = {1, 3, 5, 2+5, 3, 10, 7*2-1, 7%4, 6, 9};
 const int length = 10;
 
 void sort()
 {
-    int i, j, max, temp, length;
+    int i, j, max, temp;
+    
     i = 0; 
     j = 0;
     while (i < length)
     {
-	j = i + 1;
+	j = i;
 	max = i;
 	while (j < length)
 	{
 	    if (a[max] < a[j]) max = j;
 	    j = j + 1;
 	}
-	if (max != j)
+	if (max != i)
 	{
 	    temp = a[max];
 	    a[max] = a[i];
