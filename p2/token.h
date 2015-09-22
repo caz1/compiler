35568a64a4c55c733de51eefd7bfe60b5d66dbe2
caz <caz@mail.ustc.edu.cn>
2015-09-22 21:27:50 +0800

    .

---
+++ p2/token.h
@@ -0,0 +1,43 @@
+#ifndef TOKEN_H_
+#define TOKEN_H_
+
+
+#define NUL	0
+#define NUMBER	1
+#define ID	2
+#define PLUS	3
+#define MINUS	4
+#define MULT	5
+#define DIV	6	
+#define MOD	7
+#define EQ	8
+#define NE	9
+#define LT	10	
+#define LE	11
+#define GT	12	
+#define GE	13
+#define MAIN	14
+#define RELOP	15
+#define BINOP	16
+#define COMMA	17
+#define SEMICOLON	18
+#define LBRACKET	19
+#define RBRACKET	20
+#define LPAR	21
+#define RPAR	22
+#define LBRACE	23
+#define RBRACE	24
+#define WHILE	25
+#define IF	26	
+#define CONST	27
+#define INT	28
+#define ELSE	29
+#define RELOP	30
+
+#include<stdio.h>
+
+
+
+
+
+#endif
